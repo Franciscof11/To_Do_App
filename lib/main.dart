e2bd8b904e9f891f396d7f6055c72e5248bab035
Francisco Gabriel <franciscogabrielf11@gmail.com>
2024-08-16 23:27:44 -0300

    feat: firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:to_do_app/modules/to_do/presentation/pages/introduction_pages/onboarding_screen.dart';
+import 'package:to_do_app/firebase_options.dart';
+import 'package:to_do_app/modules/to_do/presentation/pages/home_page/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     DevicePreview(
-      enabled: true,
+      enabled: true, // Ativa ou não o Device Preview
       builder: (context) => const MainApp(),
     ),
   );
@@ -18,7 +26,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: OnBoardingScreen(),
+      home: HomePage(),
     );
   }
 }

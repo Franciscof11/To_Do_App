833c42c846ae81db2439ef29b516a817a49eb64f
Francisco Gabriel <franciscogabrielf11@gmail.com>
2024-08-19 01:51:12 -0300

    refac: splash screen

--- lib/modules/to_do/presentation/pages/splash_screen/splash_screen.dart
+++ lib/modules/to_do/presentation/pages/splash_screen/splash_screen.dart
@@ -1,13 +1,8 @@
-import 'dart:async';
-
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:page_transition/page_transition.dart';
-import 'package:to_do_app/config/database/tasks/tasks_db.dart';
 import 'package:to_do_app/modules/auth/data/auth_repository.dart';
-import 'package:to_do_app/modules/to_do/data/task_repository.dart';
-import 'package:to_do_app/modules/to_do/domain/task.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -17,30 +12,6 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  deploymentRoutine() {
-    final taskDB = TasksDB();
-
-    final tasksRepository = TaskRepository();
-
-    Timer.periodic(
-      const Duration(seconds: 5),
-      (timer) async {
-        final List<Task> tasks = await taskDB.getAllTasks();
-
-        await tasksRepository.postToFirestore(tasks: tasks);
-
-        debugPrint('#Sucess firestore deploy routine!');
-        debugPrint(tasks.toString());
-      },
-    );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    deploymentRoutine();
-  }
-
   @override
   Widget build(BuildContext context) {
     final authFirebaseRepository = AuthRepository();
@@ -48,7 +19,7 @@ class _SplashScreenState extends State<SplashScreen> {
       splash: Lottie.asset('assets/splash.json'),
       pageTransitionType: PageTransitionType.fade,
       splashTransition: SplashTransition.fadeTransition,
-      duration: 4100,
+      duration: 3900,
       backgroundColor: const Color.fromRGBO(228, 240, 246, 1),
       splashIconSize: 80000,
       nextScreen: authFirebaseRepository.authChanges(context),

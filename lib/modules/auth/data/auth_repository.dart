dd7cd1adee2326d256166339b94d19e52a9baf8d
Francisco Gabriel <franciscogabrielf11@gmail.com>
2024-08-19 20:53:41 -0300

    refac: auth repository

--- lib/modules/auth/data/auth_repository.dart
+++ lib/modules/auth/data/auth_repository.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:to_do_app/modules/auth/presentation/pages/sign_in_page.dart';
 import 'package:to_do_app/modules/to_do/data/task_repository.dart';
@@ -31,28 +30,9 @@ class AuthRepository {
     );
   }
 
-  signInWithApple() async {
-    final appleProvider = AppleAuthProvider();
+  signInWithApple() async {}
 
-    await FirebaseAuth.instance.signInWithProvider(appleProvider);
-  }
-
-  signInWithGoogle() async {
-    // Trigger the authentication flow
-    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-
-    // Obtain the auth details from the request
-    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
-
-    // Create a new credential
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth?.accessToken,
-      idToken: googleAuth?.idToken,
-    );
-
-    // Once signed in, return the UserCredential
-    return await FirebaseAuth.instance.signInWithCredential(credential);
-  }
+  signInWithGoogle() async {}
 
   void signOut() async {
     await FirebaseAuth.instance.signOut();
